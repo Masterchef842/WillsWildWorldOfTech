c0a79b9132a143901a5cb1f6654a2d8451ee5349
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-24 21:24:01 -0500

    added associations

--- models/Comment.js
+++ models/Comment.js
@@ -17,6 +17,7 @@ Comment.init(
         },
         post_id:{
             type: DataTypes.INTEGER,
+            allowNull: false,
             references:{
                 model: "blog_post",
                 key: "id"
--- models/index.js
+++ models/index.js
@@ -1,3 +1,32 @@
 const User = require ("./User");
 const Comment = require("./Comment.js");
-const blogPost=require("./blogPost")
+const blogPost=require("./blogPost");
+
+User.hasMany(blogPost,{
+    foreignKey: "user_id",
+    onDelete: 'CASCADE'
+})
+
+blogPost.belongsTo(User,{
+    foreignKey: "user_id"
+})
+
+
+User.hasMany(Comment,{
+    foreignKey: "user_id",
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(User,{
+    foreignKey: "user_id"
+})
+
+
+blogPost.hasMany(Comment,{
+    foreignKey: "post_id",
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(blogPost,{
+    foreignKey: "post_id"
+})
