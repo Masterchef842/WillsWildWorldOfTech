8bfa50f9247e40348e4750dc466fd167c4eabf09
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-24 20:55:14 -0500

    added models

---
+++ models/Comment.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: "blog_post",
+                key: "id"
+            }
+
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        }
+    },
+    {
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+
+    }
+)
+
+module.exports=Comment;
---
+++ models/index.js
@@ -0,0 +1,3 @@
+const User = require ("./User");
+const Comment = require("./Comment.js");
+const blogPost=require("./blogPost")
